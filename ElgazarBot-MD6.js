15ad123c9bdf9606e79abce673fb7e59ee28343f
nobaraaleviathan <123747315+nobaraaleviathan@users.noreply.github.com>
2023-04-21 14:36:51 +0200

    Update ElgazarBot-MD6.js

--- ElgazarBot-MD6.js
+++ ElgazarBot-MD6.js
@@ -9218,7 +9218,7 @@ let buttons = [
 {buttonId: `owner`, buttonText: {displayText: 'المطور👤'}, type: 1}
 ]
 let buttonMessage = {
-image: {url: `https://telegra.ph/file/e6259010eb14f0e5fb98d.jpg`},
+image: {url: `file:///C:/Users/Ahmed%20Abdelaal/Downloads/Naruto-Wallpaper-hd.webp`},
 jpegThumbnail: log0,
 caption: teks,
 footer: `${botname}`,
